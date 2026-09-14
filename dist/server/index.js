bacfc64e81b096b9305e8dcd2a8b193e74f7b80a
Oseias Santos <oseias01fab@gmail.com>
2026-09-14 16:17:08 -0300

    Serve Sites assets from client bundle

--- dist/server/index.js
+++ dist/server/index.js
@@ -9,11 +9,23 @@ const HTML_ROUTES = new Set([
   "/simposio-vendas",
 ]);
 
+const CLIENT_PREFIX = "/dist/client";
+
 function assetRequest(request) {
   const url = new URL(request.url);
 
   if (HTML_ROUTES.has(url.pathname) && url.pathname !== "/") {
-    url.pathname = `${url.pathname}.html`;
+    url.pathname = `${CLIENT_PREFIX}${url.pathname}.html`;
+    return new Request(url, request);
+  }
+
+  if (url.pathname === "/") {
+    url.pathname = `${CLIENT_PREFIX}/index.html`;
+    return new Request(url, request);
+  }
+
+  if (!url.pathname.startsWith("/dist/")) {
+    url.pathname = `${CLIENT_PREFIX}${url.pathname}`;
     return new Request(url, request);
   }
 
